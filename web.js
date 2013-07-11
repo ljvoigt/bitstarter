428746687309be9a228a24f4eaafe564a7acfc8c
Luiz Branco <me@luizbranco.com>
2013-07-11 18:22:31 -0300

    Fix header

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', function (err, data) {
     if (err) throw err;
-    response.write(200, {"Content-Type" : "text/html"});
+    response.writeHeader(200, {"Content-Type" : "text/html"});
     response.write(data);
     response.end();
   });
