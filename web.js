1f911570a8c53069a98ccb95c572ca2ac85080c5
Luiz Branco <me@luizbranco.com>
2013-07-11 18:13:02 -0300

    Set content header, write and close response

--- web.js
+++ web.js
@@ -6,7 +6,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', function (err, data) {
     if (err) throw err;
-    response.send(data);
+    response.write(200, {"Content-Type" : "text/html"});
+    response.write(data);
+    response.end();
   });
 });
 
